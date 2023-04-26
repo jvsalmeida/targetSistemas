fb989241169dd9b7dfd1e9cf75a2c9eeb7f7a921
João Victor Siqueira de Almeida <jvsalmeida2010@gmail.com>
2023-04-25 22:31:57 -0300

    Update questao5.c

--- questao5.c
+++ questao5.c
@@ -6,17 +6,17 @@ int main() {
     int i, j;
 
     printf("Digite uma string: ");
-    fgets(str, 1000, stdin);  // lê a string do teclado
+    fgets(str, 1000, stdin);
 
-    j = strlen(str) - 1;  // índice do último caractere
+    j = strlen(str) - 1;
 
-    for (i = 0; i < j; i++, j--) {  // percorre a string até a metade
-        temp = str[i];  // guarda o caractere atual
-        str[i] = str[j];  // substitui o caractere atual pelo correspondente no final
-        str[j] = temp;  // substitui o caractere final pelo correspondente no início
+    for (i = 0; i < j; i++, j--) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
     }
 
-    printf("String invertida: %s", str);  // imprime a string invertida
+    printf("String invertida: %s", str);
 
     return 0;
 }
